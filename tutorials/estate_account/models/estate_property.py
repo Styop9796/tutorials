e9cb7743389756c3a7e417df90719c443f7dd86f
Styop <stepan00999@gmail.com>
2024-06-02 23:40:32 +0400

    invoice creation

--- tutorials/estate_account/models/estate_property.py
+++ tutorials/estate_account/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields,models
+from odoo import Command
 
 
 
@@ -12,7 +13,17 @@ class EstatePropertyInherit(models.Model):
 
         values = {'partner_id':self.buyer_id.id,
                  'move_type' : 'out_invoice',
-                 'journal_id':1}
+                 'journal_id':1,
+                
+                 'invoice_line_ids': [
+            Command.create({
+                'name': self.name,
+                'quantity': 1,  
+                'price_unit': self.selling_price * 0.06 + 100.00, 
+                'account_id': 1 # ????????????
+            })
+        ],
+                 }
         self.env['account.move'].create(values)
         print('gogogo')
         return super().set_as_sold()
