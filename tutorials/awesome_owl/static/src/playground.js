668870c9b1fe28856474c844e4ce1be3061556a6
Styop <stepan00999@gmail.com>
2024-06-04 23:21:53 +0400

    frontend owl framework , Components

--- tutorials/awesome_owl/static/src/playground.js
+++ tutorials/awesome_owl/static/src/playground.js
@@ -1,11 +1,26 @@
 /** @odoo-module **/
 
-import { Component,useState } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
-
+import { Card} from "./card/card";
+import { TodoList } from "./todo/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter,Card, TodoList};
+    
+    setup(){
+    this.str1="<div class='text-primary'>some content</div>";
+    this.str2 = markup("<div class='text-primary'>some content</div>");
 
+
+    this.sum = useState({value:2});
+    }
+
+
+    incrementSum(){
+
+        this.sum.value++;
+    }
+ 
 }
