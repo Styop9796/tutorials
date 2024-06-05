769ad36cbc15eb033f071243fbb6112fff60fac4
Styop <stepan00999@gmail.com>
2024-06-05 14:37:03 +0400

    components done

--- tutorials/awesome_owl/static/src/card/card.js
+++ tutorials/awesome_owl/static/src/card/card.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component , useState} from "@odoo/owl";
 
 
 
@@ -10,6 +10,21 @@ export class Card extends Component {
 
     static props = {
         title: String,
-        content: String,
+        slots: {
+            type: Object,
+            shape: {
+                default: true
+            },
+        }
     };
+
+
+
+    setup() {
+        this.state = useState({ isOpen: true });
+    }
+
+    toggleContent() {
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
